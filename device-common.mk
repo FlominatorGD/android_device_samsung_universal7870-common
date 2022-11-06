29d84de8868fc7f74a789aad1a0a4121e05b389e
Flominator <github.flominatorgd@gmail.com>
2022-11-06 13:51:43 +0100

    universal7870-common: specifically target all radio targets

--- device-common.mk
+++ device-common.mk
@@ -237,7 +237,14 @@ DEVICE_PACKAGE_OVERLAYS += \
 
 # Radio
 PRODUCT_PACKAGES += \
+    android.hardware.radio.deprecated@1.0 \
+    android.hardware.radio@1.0 \
+    android.hardware.radio@1.1 \
+    android.hardware.radio@1.2 \
+    android.hardware.radio@1.3 \
+    android.hardware.radio@1.4 \
     android.hardware.radio@1.5 \
+    android.hardware.radio.config@1.1 \
     android.hardware.radio.config@1.2 \
     libprotobuf-cpp-full-vendorcompat \
     libprotobuf-cpp-lite-vendorcompat \
